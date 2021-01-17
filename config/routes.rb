d8f08c54b9801f1f21bd46c69d4735663eebdd5a
Hartley McGuire <skipkayhil@gmail.com>
2021-01-17 17:23:35 -0500

    feat: add scaffolding for repo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :repos
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'application#index'
 end
