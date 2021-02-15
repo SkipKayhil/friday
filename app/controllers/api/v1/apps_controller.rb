793c061dd532b4d8915758cdcb4bf3d465a74fb9
Hartley McGuire <skipkayhil@gmail.com>
2021-02-15 17:32:17 -0500

    feat: add apps controller, constrain wildcard path

---
+++ app/controllers/api/v1/apps_controller.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    # REST API for interacting with Apps
+    class AppsController < Api::ApplicationController
+      before_action :set_app, only: %i[show update destroy]
+
+      # GET /apps
+      def index
+        @apps = App.all
+
+        render json: @apps.as_json(include: :repo)
+      end
+
+      # GET /apps/1
+      def show
+        render json: @app.as_json(include: :repo)
+      end
+
+      # POST /apps
+      def create
+        @app = App.new(app_params)
+
+        if @app.save
+          render json: @app, status: :created, location: api_v1_app_url(@app)
+        else
+          render json: @app.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /apps/1
+      def update
+        if @app.update_dependencies
+          render json: @app
+        else
+          render json: @app.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /apps/1
+      def destroy
+        @app.destroy
+      end
+
+      private
+
+      # Use callbacks to share common setup or constraints between actions.
+      def set_app
+        @app = App.find(params[:id])
+      end
+
+      # Only allow a list of trusted parameters through.
+      def app_params
+        params.require(:app).permit
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :repos
+      resources :apps
     end
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'application#index'
-  get '/*path', to: 'application#index'
+  get '/*path', to: 'application#index', constraints: ->(req) { req.format == :html }
 end
