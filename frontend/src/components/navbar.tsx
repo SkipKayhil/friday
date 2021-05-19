da09842ebf92cf7c52e1eea51786435f1662d031
Hartley McGuire <skipkayhil@gmail.com>
2021-05-19 13:54:14 -0400

    fix: dockerfile using wrong from, update navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -8,7 +8,7 @@ const selectedClass = `bg-gray-900 text-white ${baseButtonClass}`;
 export function Navbar(): JSX.Element {
   return (
     <nav class="flex items-center py-3 border-b-2 border-gray-300">
-      <h2 class="font-bold">dep</h2>
+      <h2 class="font-bold">friday</h2>
       <ul class="flex items-center space-x-4 ml-10">
         <li>
           <Link href="/" class={linkClass} activeClass={selectedClass}>
