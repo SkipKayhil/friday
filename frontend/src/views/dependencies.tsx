09b293baab249e704afe57273ba5f07a62e2df64
Hartley McGuire <skipkayhil@gmail.com>
2021-07-02 20:15:58 -0400

    feat: add search to dependencies list

--- frontend/src/views/dependencies.tsx
+++ frontend/src/views/dependencies.tsx
@@ -1,9 +1,11 @@
 import { JSX } from "preact";
+import { useState } from "preact/hooks";
 import useSWR from "swr";
 import { route } from "preact-router";
 import { Header } from "../components/header";
 import { Spinner } from "../components/spinner";
 import { Table, Column } from "../components/table";
+import { TextField } from "../components/textField";
 import { Dependency } from "../models";
 
 const columns: Column<Dependency>[] = [
@@ -16,15 +18,26 @@ const onRowClick = ({ row }: { row: Dependency }) => {
 };
 
 export function Dependencies(): JSX.Element {
+  const [search, setSearch] = useState("");
   const { data } = useSWR<Dependency[]>("/api/v1/dependencies");
 
   if (!data) return <Spinner />;
 
+  const transformedData = data.filter((dependency) => dependency.name.includes(search));
+
   return (
     <>
-      <Header title="dependencies" />
+      <Header title="dependencies">
+        <TextField
+          id="search"
+          class="ml-auto"
+          placeholder="search"
+          value={search}
+          onInput={(e) => setSearch((e.target as HTMLInputElement).value)}
+        />
+      </Header>
       <main>
-        <Table rows={data} columns={columns} onRowClick={onRowClick} />
+        <Table rows={transformedData} columns={columns} onRowClick={onRowClick} />
       </main>
     </>
   );
