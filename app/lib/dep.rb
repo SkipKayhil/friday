652b75512df38637b6e39bef3e187b5c395307cc
Hartley McGuire <skipkayhil@gmail.com>
2021-07-17 21:56:39 -0400

    refactor: rm the old dependabot wrapper classes

--- app/lib/dep.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# Helpers for using Dependabot
-module Dep
-end
--- app/lib/dep/artifacts.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Dep
-  Artifacts = Struct.new(:package_manager, :credentials, :source, :fetcher)
-end
