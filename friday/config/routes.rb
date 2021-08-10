ce728c35b35d3d0a38eea5c99c9a14db7ae3289d
Hartley McGuire <skipkayhil@gmail.com>
2021-08-09 23:32:54 -0400

    refactor: add friday project controller

--- friday/config/routes.rb
+++ friday/config/routes.rb
@@ -3,4 +3,5 @@
 Friday::Engine.routes.draw do
   resources :dependencies, only: :index
   get '/dependencies/:language/:name', to: 'dependencies#show', as: :dependency
+  resources :projects
 end
---
+++ friday/test/controllers/friday/projects_controller_test.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module Friday
+  class ProjectsControllerTest < ActionDispatch::IntegrationTest
+    include Engine.routes.url_helpers
+
+    setup do
+      @project = friday_projects(:friday_app)
+    end
+
+    test "should get index" do
+      get projects_url, as: :json
+      assert_response :success
+    end
+
+    test "should create project" do
+      assert_difference("Project.count") do
+        post projects_url, params: {project: {directory: @project.directory, language_version: @project.language_version, name: @project.name, package_manager: @project.package_manager, repository_id: @project.repository_id}}, as: :json
+      end
+
+      assert_response 201
+    end
+
+    test "should show project" do
+      get project_url(@project), as: :json
+      assert_response :success
+    end
+
+    test "should update project" do
+      patch project_url(@project), params: {project: {directory: @project.directory, language_version: @project.language_version, name: @project.name, package_manager: @project.package_manager, repository_id: @project.repository_id}}, as: :json
+      assert_response 200
+    end
+
+    test "should destroy project" do
+      assert_difference("Project.count", -1) do
+        delete project_url(@project), as: :json
+      end
+
+      assert_response 204
+    end
+  end
+end
